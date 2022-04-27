520702db2b3c6cccc3cc5a1c613e9435a6076b6d
Anfacibry <jogos.fabriciocintra@gmail.com>
2022-04-27 16:55:09 -0300

    atualizando tela principal

---
+++ lib/pages/corpo_tela_principal.dart
@@ -0,0 +1,60 @@
+import 'package:bytebank/data/transferencia.dart';
+import 'package:bytebank/widgets/item_transferencia.dart';
+import 'package:flutter/material.dart';
+
+import '../widgets/caixa_de_texto.dart';
+
+class TelaDeCadastro extends StatelessWidget {
+  final TextEditingController numeroContaPego = TextEditingController();
+  final TextEditingController valorPego = TextEditingController();
+  TelaDeCadastro({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Tranferência"),
+        centerTitle: true,
+      ),
+      body: Column(
+        children: [
+          CaixaDeTexto(
+            numeroPego: numeroContaPego,
+            labelText: "Número da conta",
+            hintText: "1000",
+          ),
+          CaixaDeTexto(
+            icone: true,
+            numeroPego: valorPego,
+            labelText: "Valor",
+            hintText: "0.00",
+          ),
+          ElevatedButton(
+            onPressed: () {
+              final int? numeroConta = int.tryParse(numeroContaPego.text);
+              final double? valorConta = double.tryParse(valorPego.text);
+
+              if (numeroConta != null && valorConta != null) {
+                final transferenciaRecebida = ItemTransferencia(
+                    transferencia: Transferencia(
+                        numeroConta: numeroConta, valor: valorConta));
+                debugPrint("$transferenciaRecebida");
+                debugPrint("Criando Transferência");
+
+                Navigator.pop(
+                  context,
+                  transferenciaRecebida,
+                );
+                debugPrint("$transferenciaRecebida");
+              }
+            },
+            child: const Text("Confirmar"),
+            style: ElevatedButton.styleFrom(
+              primary: const Color.fromARGB(255, 99, 161, 27),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
