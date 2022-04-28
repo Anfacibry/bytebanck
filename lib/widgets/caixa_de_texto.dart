e803e811372e133086ad5e27b4b12768177be440
Anfacibry <jogos.fabriciocintra@gmail.com>
2022-04-28 09:04:50 -0300

    adicionando caixa de texto

--- lib/widgets/caixa_de_texto.dart
+++ lib/widgets/caixa_de_texto.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
 
 class CaixaDeTexto extends StatelessWidget {
+  final bool icone;
+  final TextEditingController numeroPego;
   final String labelText;
   final String hintText;
   const CaixaDeTexto({
+    this.icone = false,
+    required this.numeroPego,
     required this.labelText,
     required this.hintText,
     Key? key,
@@ -14,11 +18,19 @@ class CaixaDeTexto extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.fromLTRB(16.0, 10.0, 16.0, 0.0),
       child: TextField(
+        controller: numeroPego,
         decoration: InputDecoration(
+          icon: icone
+              ? const Icon(
+                  Icons.monetization_on,
+                  color: Color.fromARGB(255, 99, 161, 27),
+                )
+              : null,
           border: const OutlineInputBorder(),
           labelText: labelText,
           hintText: hintText,
         ),
+        keyboardType: TextInputType.number,
       ),
     );
   }
